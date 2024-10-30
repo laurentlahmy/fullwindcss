fc550aebf9ccd78c29b404f6b571bfefbc07c4af
Laurent Lahmy <lahmy.laurent@gmail.com>
2024-10-31 03:25:22 +0700

    fix types

--- index.ts
+++ index.ts
@@ -4,19 +4,19 @@ import baseColors from "tailwindcss/colors";
 // // console.log("-> ", { baseColors }, "");
 
 const tailwind_select_colors = Object.entries(baseColors).filter(
-  ([key, value]) => value["50"]
+  ([, value]) => value["50"]
 );
 // console.log("-> ", { tailwind_select_colors }, "");
 const tailwind_select_colors_with_color_array = tailwind_select_colors.map(
   ([color_name_as_string, color_object]) => {
     const current_color_array = Object.entries(color_object);
     const current_color_colors_array = current_color_array.map(
-      ([color_number_as_string, color_hex_as_string]) => {
+      ([, color_hex_as_string]) => {
         return color_hex_as_string;
       }
     );
     const current_color_values_array = current_color_array.map(
-      ([color_number_as_string, color_hex_as_string]) => {
+      ([color_number_as_string]) => {
         return Number(color_number_as_string);
       }
     );
