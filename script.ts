319d26f457c212be4fb8325b00c82d4568a9f042
Laurent Lahmy <lahmy.laurent@gmail.com>
2024-12-08 15:47:47 +0100

    save

---
+++ script.ts
@@ -0,0 +1,36 @@
+import { c } from "./index";
+
+type Color =
+  | "slate"
+  | "gray"
+  | "zinc"
+  | "neutral"
+  | "stone"
+  | "red"
+  | "orange"
+  | "amber"
+  | "yellow"
+  | "lime"
+  | "green"
+  | "emerald"
+  | "teal"
+  | "cyan"
+  | "sky"
+  | "blue"
+  | "indigo"
+  | "violet"
+  | "purple"
+  | "fuchsia"
+  | "pink"
+  | "rose"
+  | "lightBlue"
+  | "warmGray"
+  | "trueGray"
+  | "coolGray"
+  | "blueGray";
+
+export const colorScript = (color: Color = "zinc", number: number = 222) =>
+  document.documentElement.style.setProperty(
+    `--${color}${number}`,
+    c[color](number)
+  );
--- tsup.config.js
+++ tsup.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
+  entry: ["./index.ts", "./script.ts"],
   sourcemap: false,
   clean: true,
   treeshake: true,
